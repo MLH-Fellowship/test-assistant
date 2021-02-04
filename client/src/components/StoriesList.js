35917f233b9278f8880bbf060f9ff922a239b407
Aditya Sharma <sharmaaditya570191@gmail.com>
2021-02-04 13:01:30 +0530

    Update storylist component

--- client/src/components/StoriesList.js
+++ client/src/components/StoriesList.js
@@ -4,7 +4,7 @@ import { navigate, Link } from '@reach/router'
 import Vote from './Vote'
 
 const StoriesList = (props) => {
-  const { stories, state, product } = props
+  const { stories } = props
 
   const strip = (html) => {
     html = html.replace(/<\s*[^>]*>/gi, '')
@@ -18,8 +18,7 @@ const StoriesList = (props) => {
     <div className='flex flex-column'>
       {stories && stories.length ? (
         stories.map((story, key) => {
-          return story.user_story_status.Status === state &&
-            (story.product.Name === product || product === 'All') ? (
+          return (
             <div className='story' key={key}>
               <Vote story={story} />
               <div
@@ -49,10 +48,6 @@ const StoriesList = (props) => {
                   </Link>
                 </div>
               </div>
-              <div className='flex flex-column story-subcontent'>
-                <small>Category</small>
-                <span className='category-text'>{story.Category}</span>
-              </div>
               <div className='flex flex-column'>
                 <span className='story-meta'>
                   <i className='eos-icons'>attachment</i>
@@ -64,9 +59,7 @@ const StoriesList = (props) => {
                 </span>
               </div>
             </div>
-          ) : (
-            ''
-          )
+          ) 
         })
       ) : (
         <h3>No stories</h3>
